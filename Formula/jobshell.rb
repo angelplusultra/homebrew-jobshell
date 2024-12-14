0b34b36b8f9e49009dca99afa70a81c7ef573060
angelplusultra <macfittondev@gmail.com>
2024-12-13 18:11:48 -0800

    fixed sha256

--- Formula/jobshell.rb
+++ Formula/jobshell.rb
@@ -6,7 +6,7 @@ class Jobshell < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/angelplusultra/job-shell/releases/download/v1.0.4/jobshell-macos-aarch64-binary.zip"
-    sha256 "c20241e9ff5de97617e2e2527cba253f0987e192b1585089967d64b1ed6faa8"
+    sha256 "c20241e9ff5de97617e2e2527cba253f0987e192b1585089967d64b1ed6faa86"
   else
     url "https://github.com/angelplusultra/job-shell/releases/download/v1.0.4/jobshell-macos-x86_64-binary.zip"
     sha256 "28122f310fb4bb10c55a4e0e0302e64f6b81bfa604be2725653b2d7121aaa5c8"
